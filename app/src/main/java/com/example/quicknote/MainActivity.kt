dfce076cdeacc58cccca19939120231c27ce40a8
m3rcury02 <gunal2002@gmail.com>
2023-07-15 21:17:37 +0530

    Initial beta release Features : Write messages to yourself Store the notes in local storage Modern user interface

--- app/src/main/java/com/example/quicknote/MainActivity.kt
+++ app/src/main/java/com/example/quicknote/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.quicknote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quicknote.ui.theme.QuickNoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Notes(Modifier)
                 }
             }
         }
@@ -30,17 +46,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Notes(modifier: Modifier) {
+    var note by remember {
+        mutableStateOf("")
+    }
+    var notesList by remember {
+        mutableStateOf(listOf<String>())
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        LazyColumn(modifier = Modifier.weight(1f), reverseLayout = true) {
+            items(notesList) { currentNote ->
+                Text(
+                    text = currentNote, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+                Divider()
+            }
+        }
+        Row(modifier = Modifier.fillMaxWidth()) {
+            TextField(
+                value = note, onValueChange = { text ->
+                    note = text
+                },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Button(onClick = {
+                /*TODO*/
+                if (note.isNotBlank()) {
+                    notesList += note
+                    note = ""
+                }
+            }) {
+                Text(text = "Add")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     QuickNoteTheme {
-        Greeting("Android")
+        Notes(Modifier)
     }
 }
